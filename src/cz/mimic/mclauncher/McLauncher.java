e48bc3771637704820c13a5155033d22ab906397
mimic2300 <mimic2300@gmail.com>
2014-04-21 13:52:57 +0200

    - Rewrite alias - Rewrite config - Added support for Forge - Added automatic libs generation - Added system enviroment support - A much more...

--- src/cz/mimic/mclauncher/McLauncher.java
+++ src/cz/mimic/mclauncher/McLauncher.java
@@ -1,8 +1,11 @@
 package cz.mimic.mclauncher;
 
-import cz.mimic.mclauncher.base.Json;
-import cz.mimic.mclauncher.base.Minecraft;
+import cz.mimic.mclauncher.alias.AliasBuilder;
 import cz.mimic.mclauncher.config.Config;
+import cz.mimic.mclauncher.config.ConfigApplication;
+import cz.mimic.mclauncher.config.ConfigJava;
+import cz.mimic.mclauncher.config.ConfigMinecraft;
+import cz.mimic.mclauncher.json.Json;
 
 public final class McLauncher
 {
@@ -11,10 +14,27 @@ public final class McLauncher
     public static void main(String[] args)
     {
         // Json.save(JSON_FILE, new Config());
+
         Config config = Json.load(JSON_FILE);
 
+        if (config == null) {
+            return;
+        }
+        Config modifiedConfig = Json.load(JSON_FILE);
+
+        AliasBuilder.addClass(Config.class, config);
+        AliasBuilder.addClass(ConfigJava.class, config.java);
+        AliasBuilder.addClass(ConfigMinecraft.class, config.minecraft);
+        AliasBuilder.addClass(ConfigApplication.class, config.application);
+        AliasBuilder.rebuild();
+
+        // reload libs from libraries directory
+        config.minecraft.libs = Minecraft.loadLibs(config.minecraft.librariesDir);
+        modifiedConfig.minecraft.libs = config.minecraft.libs;
+        Json.save(JSON_FILE, modifiedConfig);
+
+        // start minecraft
         Minecraft mc = new Minecraft(config);
-        Thread th = new Thread(mc);
-        th.start();
+        new Thread(mc).start();
     }
 }
